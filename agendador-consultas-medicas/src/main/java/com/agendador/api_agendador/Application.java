7c5c81839d99ae506b1ccba093dbe58e4cc9c798
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-06-18 16:05:55 -0300

    first commit

---
+++ agendador-consultas-medicas/src/main/java/com/agendador/api_agendador/Application.java
@@ -0,0 +1,13 @@
+package com.agendador.api_agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ agendador-consultas-medicas/src/test/java/com/agendador/api_agendador/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendador.api_agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
