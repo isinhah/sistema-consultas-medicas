dc66bc217872539fb76eac43c5a61644ee51d0d6
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-06-19 12:59:03 -0300

    refactor(user): organize queries in UserRepository

--- src/main/java/com/agendador/api_agendador/repository/UserRepository.java
+++ src/main/java/com/agendador/api_agendador/repository/UserRepository.java
@@ -9,8 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailOrPhone(String email, String phone);
+
     Page<User> findByNameIgnoreCase(String email, Pageable pageable);
-    Page<User> findByPhone(String phone, Pageable pageable);
     Page<User> findByRole(Role role, Pageable pageable);
+
+    boolean existsByEmailAndIdNot(String email, Long id);
+    boolean existsByPhoneAndIdNot(String email, Long id);
 }
